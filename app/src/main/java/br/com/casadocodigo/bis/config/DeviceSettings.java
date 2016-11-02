e1d13f2f1332f4c5e72256b1c3bd2e13288fb025
José <jose.silva.br@outlook.com>
2016-11-02 01:07:30 -0200

    Criação da classe DeviceSettings.

---
+++ app/src/main/java/br/com/casadocodigo/bis/config/DeviceSettings.java
@@ -0,0 +1,29 @@
+package br.com.casadocodigo.bis.config;
+
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.types.CGPoint;
+import org.cocos2d.types.CGSize;
+
+/**
+ * Created by jose on 02/11/2016.
+ */
+
+public class DeviceSettings {
+
+    public static CGPoint screenResolution(CGPoint gcPoint) {
+        return gcPoint;
+    }
+
+    public static float screenWidth() {
+        return winSize().width;
+    }
+
+    public static float screenHeight() {
+        return winSize().height;
+    }
+
+    private static CGSize winSize() {
+        return CCDirector.sharedDirector().winSize();
+    }
+
+}
